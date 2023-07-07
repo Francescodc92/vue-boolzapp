550a3b00d97ceca37ba3ce70b18d8e342de794fd
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-07-07 23:16:27 +0200

    add delete function (end milestone 5)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 /*
--ricordare di modificare la lente di ringrandimento della barra di ricerca dei contatti 
 - aggiungere la funzione per trattare la data 
 - aggiungere la condizione che mostra un elemento alternativo quando nessuno dei contatti è attivo
 */
@@ -20,17 +19,20 @@ createApp({
             {
               date: '10/01/2020 15:30:55',
               message: 'Hai portato a spasso il cane?',
-              status: 'sent'
+              status: 'sent',
+              modalVisible: false
             },
             {
               date: '10/01/2020 15:50:00',
               message: 'Ricordati di stendere i panni',
-              status: 'sent'
+              status: 'sent',
+              modalVisible:false,
             },
             {
               date: '10/01/2020 16:15:22',
               message: 'Tutto fatto!',
-              status: 'received'
+              status: 'received',
+              modalVisible:false,
             }
           ],
         },
@@ -42,17 +44,20 @@ createApp({
             {
               date: '20/03/2020 16:30:00',
               message: 'Ciao come stai?',
-              status: 'sent'
+              status: 'sent',
+              modalVisible:false,
             },
             {
               date: '20/03/2020 16:30:55',
               message: 'Bene grazie! Stasera ci vediamo?',
-              status: 'received'
+              status: 'received',
+              modalVisible:false,
             },
             {
               date: '20/03/2020 16:35:00',
               message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-              status: 'sent'
+              status: 'sent',
+              modalVisible:false,
             }
           ],
         },
@@ -64,17 +69,20 @@ createApp({
             {
               date: '28/03/2020 10:10:40',
               message: 'La Marianna va in campagna',
-              status: 'received'
+              status: 'received',
+              modalVisible:false,
             },
             {
               date: '28/03/2020 10:20:10',
               message: 'Sicuro di non aver sbagliato chat?',
-              status: 'sent'
+              status: 'sent',
+              modalVisible:false,
             },
             {
               date: '28/03/2020 16:15:22',
               message: 'Ah scusa!',
-              status: 'received'
+              status: 'received',
+              modalVisible:false,
             }
           ],
         },
@@ -86,12 +94,14 @@ createApp({
             {
               date: '10/01/2020 15:30:55',
               message: 'Lo sai che ha aperto una nuova pizzeria?',
-              status: 'sent'
+              status: 'sent',
+              modalVisible:false,
             },
             {
               date: '10/01/2020 15:50:00',
               message: 'Si, ma preferirei andare al cinema',
-              status: 'received'
+              status: 'received',
+              modalVisible:false,
             }
           ],
         },
@@ -103,12 +113,14 @@ createApp({
             {
               date: '10/01/2020 15:30:55',
               message: 'Ricordati di chiamare la nonna',
-              status: 'sent'
+              status: 'sent',
+              modalVisible:false,
             },
             {
               date: '10/01/2020 15:50:00',
               message: 'Va bene, stasera la sento',
-              status: 'received'
+              status: 'received',
+              modalVisible:false,
             }
           ],
         },
@@ -120,17 +132,20 @@ createApp({
           {
             date: '10/01/2020 15:30:55',
             message: 'Ciao Claudia, hai novità?',
-            status: 'sent'
+            status: 'sent',
+            modalVisible:false,
           },
           {
             date: '10/01/2020 15:50:00',
             message: 'Non ancora',
-            status: 'received'
+            status: 'received',
+            modalVisible:false,
           },
           {
             date: '10/01/2020 15:51:00',
             message: 'Nessuna nuova, buona nuova',
-            status: 'sent'
+            status: 'sent',
+            modalVisible:false,
           }
         ],
         },
@@ -142,12 +157,14 @@ createApp({
             {
               date: '10/01/2020 15:30:55',
               message: 'Fai gli auguri a Martina che è il suo compleanno!',
-              status: 'sent'
+              status: 'sent',
+              modalVisible :false,
             },
             {
               date: '10/01/2020 15:50:00',
               message: 'Grazie per avermelo ricordato, le scrivo subito!',
-              status: 'received'
+              status: 'received',
+              modalVisible :false,
             }
           ],
         },
@@ -159,17 +176,20 @@ createApp({
             {
               date: '10/01/2020 15:30:55',
               message: 'Ciao, andiamo a mangiare la pizza stasera?',
-              status: 'received'
+              status: 'received',
+              modalVisible: false,
             },
             {
               date: '10/01/2020 15:50:00',
               message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-              status: 'sent'
+              status: 'sent',
+              modalVisible: false,
             },
             {
               date: '10/01/2020 15:51:00',
               message: 'OK!!',
-              status: 'received'
+              status: 'received',
+              modalVisible: false,
             }
           ],
         },
@@ -181,63 +201,21 @@ createApp({
             {
               date: '10/01/2020 15:30:55',
               message: 'Ciao, andiamo a mangiare la pizza stasera?',
-              status: 'received'
+              status: 'received',
+              modalVisible :false,
             },
             {
               date: '10/01/2020 15:50:00',
               message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-              status: 'sent'
+              status: 'sent',
+              modalVisible :false,
             },
             {
               date: '10/01/2020 15:51:00',
               message: 'OK!!',
-              status: 'received'
-            },
-            {
-              date: '10/01/2020 15:30:55',
-              message: 'Ciao, andiamo a mangiare la pizza stasera?',
-              status: 'received'
-            },
-            {
-              date: '10/01/2020 15:50:00',
-              message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-              status: 'sent'
-            },
-            {
-              date: '10/01/2020 15:51:00',
-              message: 'OK!!',
-              status: 'received'
-            },
-            {
-              date: '10/01/2020 15:30:55',
-              message: 'Ciao, andiamo a mangiare la pizza stasera?',
-              status: 'received'
-            },
-            {
-              date: '10/01/2020 15:50:00',
-              message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-              status: 'sent'
-            },
-            {
-              date: '10/01/2020 15:51:00',
-              message: 'OK!!',
-              status: 'received'
-            },
-            {
-              date: '10/01/2020 15:30:55',
-              message: 'Ciao, andiamo a mangiare la pizza stasera?',
-              status: 'received'
-            },
-            {
-              date: '10/01/2020 15:50:00',
-              message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-              status: 'sent'
-            },
-            {
-              date: '10/01/2020 15:51:00',
-              message: 'OK!!',
-              status: 'received'
-            },
+              status: 'received',
+              modalVisible :false,
+            }
           ],
         }
       ]
@@ -276,10 +254,21 @@ createApp({
     },
     getDate(index){
       // this.contacts[index].messages[this.contacts[index].messages.lenght - 1].date
-      const currentContact = this.contacts[index]
-      const lastMessageId = currentContact.messages[currentContact.messages.length - 1 ].date 
+      if(this.contacts[index].messages.length > 0){
+        const currentContact = this.contacts[index]
+        const lastMessageId = currentContact.messages[currentContact.messages.length - 1 ].date 
+        
+        return lastMessageId
+      }
+      return null
       
-      return lastMessageId
+    },
+    openModal(index){
+      this.contacts[this.contactActiveIndex].messages[index].modalVisible = !this.contacts[this.contactActiveIndex].messages[index].modalVisible
+      
+    },
+    deleteMSG(index){
+      this.contacts[this.contactActiveIndex].messages.splice(index, 1)
     }
   }
 }).mount('#app');
